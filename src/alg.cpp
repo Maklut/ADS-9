0a02eaf3dbf8428e80ed438be4d4448249bea444
Egor <149178595+Maklut@users.noreply.github.com>
2026-06-05 22:52:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2022 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include <vector>
 #include "tree.h"
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <vector>
 
 int factorial(int n) {
     int result = 1;
