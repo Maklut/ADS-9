29b318948946eaba4edec473a7028276dd4312ad
Egor <149178595+Maklut@users.noreply.github.com>
2026-06-05 21:48:47 +0300

    Delete test/AllTests.cpp

--- test/AllTests.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 NNTU-CS
-#include "tree.h"
-#include <gtest/gtest.h>
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
