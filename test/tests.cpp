61efe6d7fccd852194bc85dad01a5e3ca5ad86c2
Egor <149178595+Maklut@users.noreply.github.com>
2026-06-05 22:46:00 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,41 +1,30 @@
 // Copyright 2025 NNTU-CS
-#include "tree.h"
 #include <gtest/gtest.h>
+#include <string>
 #include <vector>
 
+#include "tree.h"
+
 TEST(ads9, test1) {
-    std::vector<char> in = {'1', '2', '3'};
-    PMTree tree(in);
-    std::vector<char> result = getPerm1(tree, 1);
-    ASSERT_EQ(result.size(), 3);
-    EXPECT_EQ(result[0], '1');
-    EXPECT_EQ(result[1], '2');
-    EXPECT_EQ(result[2], '3');
+  PMTree tree(std::vector<char>{'1', '2', '3'});
+  std::vector<char> result = getPerm1(tree, 1);
+  ASSERT_TRUE(result[0] == '1' && result[1] == '2' && result[2] == '3');
 }
 
 TEST(ads9, test2) {
-    std::vector<char> in = {'1', '2', '3'};
-    PMTree tree(in);
-    std::vector<char> result = getPerm2(tree, 2);
-    ASSERT_EQ(result.size(), 3);
-    EXPECT_EQ(result[0], '1');
-    EXPECT_EQ(result[1], '3');
-    EXPECT_EQ(result[2], '2');
+  PMTree tree(std::vector<char>{'1', '2', '3'});
+  std::vector<char> result = getPerm2(tree, 2);
+  ASSERT_TRUE(result[0] == '1' && result[1] == '3' && result[2] == '2');
 }
 
 TEST(ads9, test3) {
-    std::vector<char> in = {'1', '2', '3'};
-    PMTree tree(in);
-    std::vector<char> result = getPerm1(tree, 6);
-    ASSERT_EQ(result.size(), 3);
-    EXPECT_EQ(result[0], '3');
-    EXPECT_EQ(result[1], '2');
-    EXPECT_EQ(result[2], '1');
+  PMTree tree(std::vector<char>{'1', '2', '3'});
+  std::vector<char> result = getPerm1(tree, 6);
+  ASSERT_TRUE(result[0] == '3' && result[1] == '2' && result[2] == '1');
 }
 
 TEST(ads9, test4) {
-    std::vector<char> in = {'1', '2', '3'};
-    PMTree tree(in);
-    std::vector<char> result = getPerm2(tree, 8);
-    ASSERT_EQ(result.size(), 0);
+  PMTree tree(std::vector<char>{'1', '2', '3'});
+  std::vector<char> result = getPerm2(tree, 8);
+  ASSERT_EQ(result.size(), 0);
 }
